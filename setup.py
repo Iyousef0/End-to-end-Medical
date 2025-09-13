084af2a81ae797c7f8182816f388114b9095c7a9
Yousef <yousef@example.com>
2025-09-14 02:14:25 +0400

    folder structure added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    
+
+    name="generative_ai_project",  # Use underscores or single hyphens
+    version = '0.0.1',
+    author = 'Yousef Nasser',
+    author_email = 'yousefnasser228@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
